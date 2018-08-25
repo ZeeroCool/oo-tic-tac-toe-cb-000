bcb543d1d00249c45a579e5f23b6b8294c728cdd
David Z <davidzabran@gmail.com>
2018-08-25 00:31:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,6 +20,11 @@ class TicTacToe
   puts " #{@board[0] } | #{@board[1] } | #{@board[2] } \n-----------\n #{@board[3] } | #{@board[4] } | #{@board[5] } \n-----------\n #{@board[6] } | #{@board[7] } | #{@board[8] } "
   end
 
+  def input_to_index(input = "1")
+   input.to_i - 1
+  end
+
+
 
 
 end
