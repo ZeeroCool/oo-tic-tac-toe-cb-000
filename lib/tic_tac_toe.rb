cdd08d1d14753543714de6055ed8e17b6f392d5e
David Z <davidzabran@gmail.com>
2018-08-25 00:30:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,8 +16,8 @@ class TicTacToe
   [2,4,6]  # Top Right diagonal
   ]
 
-  def display_board(board)
-  puts " #{board[0] } | #{board[1] } | #{board[2] } \n-----------\n #{board[3] } | #{board[4] } | #{board[5] } \n-----------\n #{board[6] } | #{board[7] } | #{board[8] } "
+  def display_board
+  puts " #{@board[0] } | #{@board[1] } | #{@board[2] } \n-----------\n #{@board[3] } | #{@board[4] } | #{@board[5] } \n-----------\n #{@board[6] } | #{@board[7] } | #{@board[8] } "
   end
 
 
